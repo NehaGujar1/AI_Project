7aa5f269c48bf297c5d386d16ddf5cdc93462102
NehaGujar1 <gujarnehap@gmail.com>
2023-07-04 13:14:05 +0530

    Chnages

---
+++ ml model/all.py
@@ -0,0 +1,25 @@
+import .wildfire
+import .earthquake
+import .hurricane
+import .tornado
+import .tsunami
+
+def main():
+    ans = input("Would you like to know if any there's any danger due to possible natural disasters around you? \nEnter Y to continue and N to exit")
+    if (ans == 'N') :
+        print("Thank you for checking out\n")
+        return 
+    else :
+        val = input("Enter:\n1 for wildfire\n 2 for earthquake\n 3 for hurricane\n 4 for tornado\n 5 for tsunami\n 6 to exit")
+        if ( val == 6) :
+            print("Thank you for checking out\n")
+            return 
+        elif ( val == 5 ):
+            
+        elif ( val == 4 ):
+
+        elif ( val == 3 ):
+
+        elif ( val == 2 ):   
+
+        else :
